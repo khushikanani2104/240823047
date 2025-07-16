e8e7170b1dd749903c356174784f777944a2949d
khushi knani <khushupatel2104@gmail.com>
2025-07-16 13:28:48 +0530

    updates

--- eo.js
+++ eo.js
@@ -1,9 +1,82 @@
-function eodd(a)
-{
-if(a%2==0){
-    console.log("even");
-}else{
-    console.log("odd");
-}
-}
-export default eodd;
+//1
+// function eodd(a)
+// {
+// if(a%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+// }
+// export default eodd;
+
+//2
+// function second(a,b)
+// {
+// if(a>b){
+//     console.log("a is big");
+// }else{
+//     console.log("b is big");
+// }
+// }
+// export default second;
+
+
+//3
+// function third(a)
+// {
+//     if(a%4==0){
+//         console.log("leap year");
+//     }else{
+//         console.log("not leap year");
+//     }
+//     }
+// export default third;
+
+// //4
+// function fourth(a)
+// { 
+//     let sum=0;
+//     for (let i = 0; i <= a; i++) {
+//         sum=sum+i;
+//     }
+//     console.log(sum);
+//     }
+// export default fourth;
+
+// //5
+// function five(a)
+// { 
+//     let fact=1;
+//     for (let i = 1; i <= a; i++) {
+//        fact=fact*i;
+//     }
+//     console.log(fact);
+//     }
+// export default five;
+
+// //6
+// function six(a)
+// { 
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${a} x ${i} = ${a * i}`)
+//     }
+// }
+// export default six;
+
+// //7
+// function seven(a) { 
+//     return Number(a.toString().split('').reverse().join(''));
+// }
+// export default seven;
+
+// //8
+// function eight(num) {
+//     const str = num.toString();
+//     const reversed = str.split('').reverse().join('');
+//     if (str === reversed) {
+//       return 'Palindrome';
+//     } else {
+//       return 'Not Palindrome';
+//     }
+//   }
+// export default eight;
--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // console.log("hellooooo")
 
 //-------1
-import eodd from "./eo.js"
-console.log(eodd(7));
+// import eodd from "./eo.js"
+// console.log(eodd(7));
 
 //-------2
+// import second from "./eo.js"
+// console.log(second(5,9));
+
+// //-------3
+// import third from "./eo.js"
+// console.log(third(2020));
+
+// //-------4
+// import fourth from "./eo.js"
+// console.log(fourth(5));
+
+// //-------5
+// import five from "./eo.js"
+// console.log(five(4));
+
+// //-------6
+// import six from "./eo.js"
+// console.log(six(4));
+
+// //-------7
+// import seven from "./eo.js";
+// console.log(seven(4321));
+
+// //-------8
+// import eight from "./eo.js";
+// console.log(eight(121));
+
